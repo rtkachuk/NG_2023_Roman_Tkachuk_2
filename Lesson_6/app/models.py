6996c3ab5bd2a69adba201b62abc7fb606856084
Роман Ткачук <tkachukroman64@gmail.com>
2023-11-19 21:35:19 +0200

    Implemented Messages column migration for Lesson 6

--- Lesson_6/app/models.py
+++ Lesson_6/app/models.py
@@ -4,3 +4,9 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), unique = True)
     password = db.Column(db.String(256))
+
+class Messages(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    userId = db.Column(db.Integer)
+    message = db.Column(db.String(256))
+    messageDate = db.Column(db.DateTime)
